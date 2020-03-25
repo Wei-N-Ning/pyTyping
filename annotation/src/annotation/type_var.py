021319ead78ffb99d49f09350146b22a088bda0f
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-03-25 21:05:34 +1100

    wip

---
+++ annotation/src/annotation/type_var.py
@@ -0,0 +1,5 @@
+# source:
+# functional python programming P/70
+# the type variable T_ created with TypeVar function is used
+# to clarify precisely how ....
+# ** Otherwise I run into Any to Any problem!
