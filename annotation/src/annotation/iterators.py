83f07bc53dae15c157da2a2923647d52495f6ff0
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-07-21 19:42:14 +1000

    anno iterators

--- annotation/src/annotation/iterators.py
+++ annotation/src/annotation/iterators.py
@@ -1,2 +1,24 @@
 # source: https://www.programcreek.com/python/example/94066/typing.Iterator
 # see also: pyFunctional/generators
+
+from typing import Iterator, List
+
+def get_options_to_test(self, tokens):  # type: (Iterator[str]) -> List[str]
+        options_to_test = []
+        token = next(tokens, None)
+
+        while token:
+            # "--" stops option parsing
+            if token == "--":
+                break
+
+            if token[:1] and token[0] == "-":
+                if token[:2] == "--" and len(token) > 2:
+                    options_to_test.append(token[2:])
+                elif len(token) == 2:
+                    options_to_test.append(token[1:])
+
+            token = next(tokens, None)
+
+        return options_to_test 
+
