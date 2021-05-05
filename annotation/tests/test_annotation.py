5151079b6daa8c9f65ee863e0f52e530c0bc57c3
Wei Ning (DA) <wei.ning@digitalasset.com>
2021-05-06 09:16:49 +1000

    dataclass

--- annotation/tests/test_annotation.py
+++ annotation/tests/test_annotation.py
@@ -2,10 +2,10 @@ from annotation import primitives
 from annotation import classes
 from annotation import functions
 from annotation import containers
-from annotation import aliases
+from annotation import aliases_references
 from annotation import immutable
-
 import unittest
+import dataclasses
 
 
 class TestAnnotation(unittest.TestCase):
@@ -21,3 +21,7 @@ class TestAnnotation(unittest.TestCase):
     def test_immutable(self):
         ct = immutable.Contract('doom', 'e1m1')
         self.assertEqual('doom at e1m1', ct())
+
+    def test_dataclass(self):
+        dc = classes.InventoryItem('a', 1.0, 1)
+        self.assertEqual(dataclasses.astuple(dc), ('a', 1.0, 1))
