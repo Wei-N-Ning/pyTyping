4a267159bf7fafdb5495f506719c30eb72d8220f
Wei Ning (DA) <wei.ning@digitalasset.com>
2021-05-06 08:36:44 +1000

    data class

--- annotation/src/annotation/classes.py
+++ annotation/src/annotation/classes.py
@@ -1,6 +1,8 @@
 # source: mypy cheatsheet
 # https://mypy.readthedocs.io/en/stable/cheat_sheet_py3.html
 
+from dataclasses import dataclass
+
 
 class MyClass:
     # You can optionally declare instance variables in the class body
@@ -16,3 +18,19 @@ class MyClass:
     # For instance methods, omit type for "self"
     def my_method(self, num: int, str1: str) -> str:
         return num * str1
+
+
+@dataclass
+class InventoryItem:
+    """Class for keeping track of an item in inventory."""
+    name: str
+    unit_price: float
+    quantity_on_hand: int = 0
+
+    # __init__ is automatically added to the class
+
+    # Note that this method is automatically added to the class: 
+    # #it is not directly specified in the InventoryItem definition 
+
+    def total_cost(self) -> float:
+        return self.unit_price * self.quantity_on_hand
